f516092542dd1f5c8921b8615765f500a81e9a8d
Stevestohl <stevegerring@gmail.com>
2026-06-16 10:00:55 -0700

    commit

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express()
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use('/', routes)
+app.use('/api', routes)
 
 app.get('*', (req, res) => {
     res.sendFile(path.resolve('public/index.html'))
